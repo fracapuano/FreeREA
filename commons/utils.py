8c3f26d5bae1c933f19d6e67b66f54ae63f22f3e
fracapuano <francesco_capuano@aol.com>
2022-12-17 16:53:15 +0100

    added function to check whether or not a given string could be valid as architecture

--- commons/utils.py
+++ commons/utils.py
@@ -5,6 +5,7 @@ import pickle
 import numpy as np
 import pandas as pd
 import gzip
+from itertools import chain
 
 
 def get_project_root(): 
@@ -12,6 +13,18 @@ def get_project_root():
     Returns project root directory from this script nested in the commons folder.
     """
     return Path(__file__).parent.parent
+
+def check_architecture(input_str:str)->bool: 
+    """Checks if the format of a given architecture is valid for the NATS Bench topology space"""
+    all_ops = {'none', 'nor_conv_3x3', 'avg_pool_3x3', 'skip_connect', 'nor_conv_1x1'}
+    all_numbers = {'0', '1', '2'}
+
+    subcells = input_str.split("+")  # divide the input string into different levels
+    ops = chain(*[subcell.split("|")[1:-1] for subcell in subcells])  # divide into different nodes to retrieve ops
+    subops = chain(*[op.split("~") for op in ops]) # divide into operation and node
+
+    is_valid = all([(n in all_ops) or (n in all_numbers) for n in subops]) # check if the full string is valid
+    return is_valid
 
 
 def load_metrics(dataset_metric_folder: str, include_accuracies: bool = False, include_times: bool = True):
