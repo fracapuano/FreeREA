36d7463daf4a827c9515262e2683983f604d855e
fracapuano <francesco_capuano@aol.com>
2023-02-21 18:37:58 +0100

    add: args to execute script

--- compute_metrics.py
+++ compute_metrics.py
@@ -2,8 +2,26 @@
 from commons import *
 from metrics import *
 from tqdm import tqdm
+from multiprocessing import Pool
 
-import time
+import argparse
+
+
+def parse_args()->object: 
+    """Args function. 
+    Returns:
+        (object): args parser
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", default="cifar10", type=str, help="Dataset to be used in computing the considered metrics")
+    parser.add_argument("--path-to-save", default="cachedmetrics", type=str, help="Where to save the cached-metrics files")
+
+    return parser.parse_args()
+
+args = parse_args()
+
+this_dataset = args.dataset
+cachedmetrics_path = args.path_to_save
 
 def score_all_nets(dataset:str="cifar10", metrics:list=all_metrics, path_to_save:str="cachedmetrics"): 
     """Score all networks inside NATS-Bench for the same set of three metrics"""
@@ -11,8 +29,10 @@ def score_all_nets(dataset:str="cifar10", metrics:list=all_metrics, path_to_save
     result = np.zeros(shape=(len(nats), 1+len(metrics)))
     # taking a random batch of images
     images = Dataset(name=dataset, batchsize=64).random_examples(with_labels=False)
+    p_bar = tqdm(nats)
+    p_bar.set_description(f"Dataset: {dataset}")
 
-    for net_idx, net in enumerate(tqdm(nats)):
+    for net_idx, net in enumerate(p_bar):
         # storing the metrics
         result[net_idx, :] = [net_idx] + [
             metric(net=metric_interface(metric, net), inputs=images) for metric in metrics
@@ -21,7 +41,7 @@ def score_all_nets(dataset:str="cifar10", metrics:list=all_metrics, path_to_save
     np.savetxt(f"{path_to_save}/{dataset}_cachedmetrics.txt", result, header="Arch_Idx, NASWOT, logSynflow, PortionSkipped")
 
 def main():
-    score_all_nets(dataset="ImageNet16-120")
+    score_all_nets(dataset=this_dataset, path_to_save=cachedmetrics_path)
 
 if __name__ == "__main__":
     main()
