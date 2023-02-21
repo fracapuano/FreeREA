c223f1fe099ee7a4c3636597da3068e6834ad501
fracapuano <francesco_capuano@aol.com>
2023-02-21 18:16:26 +0100

    add: arch index saved

--- compute_metrics.py
+++ compute_metrics.py
@@ -5,24 +5,23 @@ from tqdm import tqdm
 
 import time
 
-def score_all_nets(dataset:str="cifar10", metrics:list=all_metrics): 
+def score_all_nets(dataset:str="cifar10", metrics:list=all_metrics, path_to_save:str="cachedmetrics"): 
     """Score all networks inside NATS-Bench for the same set of three metrics"""
     nats = NATSInterface(dataset=dataset)
-    result = np.zeros(shape=(len(nats), len(metrics)))
+    result = np.zeros(shape=(len(nats), 1+len(metrics)))
     # taking a random batch of images
-    images = Dataset(name=dataset).random_examples(with_labels=False)
+    images = Dataset(name=dataset, batchsize=64).random_examples(with_labels=False)
 
     for net_idx, net in enumerate(tqdm(nats)):
-        start = time.time()
-        result[net_idx, :] = [
+        # storing the metrics
+        result[net_idx, :] = [net_idx] + [
             metric(net=metric_interface(metric, net), inputs=images) for metric in metrics
             ]
-        compute_time = time.time() - start
-        print(f"Time to score one candidate: {compute_time}")
-        break
+        
+    np.savetxt(f"{path_to_save}/{dataset}_cachedmetrics.txt", result, header="Arch_Idx, NASWOT, logSynflow, PortionSkipped")
 
 def main():
-    score_all_nets()
+    score_all_nets(dataset="ImageNet16-120")
 
 if __name__ == "__main__":
     main()
