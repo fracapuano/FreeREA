256e2fd231f6d3080ff78a29d727bcd40ef8363d
mttmtt31 <matteomatteotti1@gmail.com>
2023-03-09 22:31:15 +0100

    fix: typo

--- commons/utils.py
+++ commons/utils.py
@@ -5,16 +5,16 @@ import numpy as np
 import random
 import pickle
 
-def load_images(dataset:str='cifar100', batchsize:int=32):
+def load_images(dataset:str='cifar100', batch_size:int=32):
     if dataset not in ["cifar10", "cifar100", "imagenet"]:
         if 'imagenet' not in dataset.lower():
             raise ValueError('Please specify a valid dataset. Should be one of cifar10, cifar100, ImageNet')
         else:
             dataset = 'imagenet'
-    if batchsize not in [32, 64]:
-            raise ValueError(f"Batch size: {batchsize} not accepted. Can only be 32 or 64.")
+    if batch_size not in [32, 64]:
+            raise ValueError(f"Batch size: {batch_size} not accepted. Can only be 32 or 64.")
     random_batch = random.randrange(10)
-    with open(f'data/{dataset}__batch{batchsize}_{random_batch}', 'rb') as pickle_file:
+    with open(f'data/{dataset}__batch{batch_size}_{random_batch}', 'rb') as pickle_file:
         print(f'Batch #{random_batch} loaded.')
         images = pickle.load(pickle_file)
 
