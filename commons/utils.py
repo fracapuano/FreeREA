ab2b2bcc2733f392759b3e18f96560d2ef4efdf6
fracapuano <francesco_capuano@aol.com>
2023-04-18 15:38:03 +0200

    add: seeding for reproducibility

--- commons/utils.py
+++ commons/utils.py
@@ -166,3 +166,11 @@ def init_model(model):
     """Applies kaiming normal weights initialization to input model."""
     model.apply(kaiming_normal)
     return model
+
+def seed_all(seed:int):
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
